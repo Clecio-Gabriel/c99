7bbd2b6491a6ebfeed32d44c3cc610ee2efabb55
Clecio-Gabriel <cleciogabofelix@gmail.com>
2025-09-22 22:34:29 -0300

    started 2848 and 3074, both from lista5

---
+++ lista5/3074.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int queries_in_array(int q);
+
+int main(){
+
+    int tam;
+    scanf("%i\n",&tam);
+
+    int vet[tam];
+    for (int i = 0; i<tam; i++){
+        scanf("%i",&vet[i]);
+    }
+    int q;
+    scanf("%i", &q);
+
+
+}
