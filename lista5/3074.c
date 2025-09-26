45680f9fb5aae5cc2c30d98a109f3a364bd9051e
Clecio-Gabriel <cleciogabofelix@gmail.com>
2025-09-26 00:17:22 -0300

    perdi o prazo, finalizar depois

--- lista5/3074.c
+++ lista5/3074.c
@@ -1,18 +1,45 @@
 #include <stdio.h>
 
-int queries_in_array(int q);
+const int T = 20;
+
+void queries(int nQ, char pedido[T], int tam, int vet[]);
 
 int main(){
 
-    int tam;
-    scanf("%i\n",&tam);
+    int N;
+    scanf("%i",&N);
 
-    int vet[tam];
-    for (int i = 0; i<tam; i++){
+    int vet[N];
+    for (int i = 0; i<N; i++){
         scanf("%i",&vet[i]);
     }
-    int q;
-    scanf("%i", &q);
+
+    int Q;
+    scanf("%i", &Q);
+    getchar();
+
+    char pedido[T];
+    for (int i = 0; i<Q; i++){
+        fgets(pedido, 18, stdin);
+        queries(i, pedido, N, vet);
+    }
+
+}
+
+void queries(int nQ, char pedido[T], int tam, int vet[]){
+    char op[7];
+    int i, j, k;
+    int coord;
+    for (int i = 0; i<7; i++){
+        if (pedido[i]==' '){
+            op[i] = '\0';
+            int coord = i;
+            break;
+        }
+        op[i] = pedido[i];
+    }
+    
+    
 
 
 }
