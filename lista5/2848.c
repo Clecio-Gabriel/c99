dd245f392b46e940e5f50f068a82688685224c8c
Clecio-Gabriel <cleciogabofelix@gmail.com>
2025-09-25 23:01:58 -0300

    finalized 2848

--- lista5/2848.c
+++ lista5/2848.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-void proc_q(int vet_res[5], int tam, int vet_ori[tam], int l, char mat_res[][3]);
+void proc_q(int vet_res[5], int tam, int vet_ori[tam], int l);
 void ord_vet(int tam, int vet[tam]);
 int ext_menor(int coord, int tam, int vet[tam]);
-void pass_res(char vet[3], int l, char mat[][3]);
+int module(int a);
 
 int main(){
 
@@ -13,55 +13,93 @@ int main(){
     for (int i = 0; i<N; i++){
         scanf("%i", &vet[i]);
     }
-    char result[Q][3];
+    getchar();
     
-    int vet_ent[5];
+    int vet_ent[Q][5];
     for (int i = 0; i<Q; i++){
         for (int j = 0; j<5; j++){
-            scanf("%i", &vet_ent[i]);
+            scanf("%i", &vet_ent[i][j]);
         }
-        proc_q(vet_ent, N, vet, i, result);
     }
-    
 
-
+    //PRINT
+    for (int i = 0; i<Q; i++){ 
+        proc_q(vet_ent[i], N, vet, i);
+    }
     
     return 0;
 }
 
-void proc_q(int vet_res[5], int tam, int vet_ori[tam], int l, char mat_res[][3]){
+void proc_q(int vet_res[5], int tam, int vet_ori[tam], int l){
     int L = vet_res[0]-1; int R = vet_res[1]-1; 
     int K = vet_res[2]; //Coordenada do menor termo
     int G = vet_res[3]; int D = vet_res[4];
     
-    int kmenor = ext_menor(K, tam, vet_ori);
-
-
-
-    /*
-    1 - ORDENAR TERMO DE FORMA CRESCENTE(para usar o K como coordenada no vetor original);
-    2 - EXTRAIR O K-ENÉSIMO MENOR TERMO;
-    */
-
-    char result[3];
-    
-    pass_res(result, l, mat_res);
-}
-
-int ext_menor(int coord, int tam, int vet[tam]){
-    ord_vet(tam, vet);
-    int menor, count;
-    /*
-    1 - CHECAR QUAL É O K-nésimo termo
-    */
-}
-
-void pass_res(char vet[3], int l, char mat[][3]){
-    for (int i = 0; i<3; i++){
-        mat[l][i] = vet[i];
+    //1º passo
+    int Ttemp = (R-L)+1; 
+    int vet_temp[Ttemp];
+    int j = 0;
+    for (int i = L; i<=R; i++){
+        vet_temp[j] = vet_ori[i];
+        j++;
     }
+    
+    //2º passo
+    int kmenor = ext_menor(K, Ttemp, vet_temp); //ISSO VAI ORDENAR O vet_temp
+    int count = 0;
+    for (int i = 0; i<Ttemp; i++){
+        if (vet_temp[i]==kmenor){
+            count++;
+        }
+    }
+
+    //3º passo
+    int dG = module(G-count);
+    int dD = module(D-count);
+    char win;
+    if (dG<dD){
+        win = 'G';
+    }
+    else if (dD<dG){
+        win = 'D';
+    }
+    else{
+        win = 'E' ;
+    }
+
+    //4º passo
+    /*
+    O QUE PRECISA PRO PRINT:
+    kmenor - count - vencedor
+    */
+    printf("%i %i %c\n", kmenor, count, win);
+
+    /*
+    1 - CRIAR vetor_temp;✓
+    2 - EXTRAIR O K-ENÉSIMO MENOR TERMO E FAZER SUA CONTAGEM;✓
+    3 - VER QUEM FICA MAIS PRÓXIMO E DETERMINAR O VENCEDOR;✓
+    4 - PREPARAR O result E PASSAR PARA MATRIZ;✓
+    */
 }
 
+//FINALIZADO
+int module(int a){
+    if (a<0){
+        a*=-1;
+    }
+    return a;
+}
+
+//FINALIZADA
+int ext_menor(int coord, int tam, int vet[tam]){
+    
+    ord_vet(tam, vet);
+    int menor = vet[coord-1];
+    return menor;
+
+}
+
+//FINALIZADA
 void ord_vet(int tam, int vet[tam]){
     
     int temp;
