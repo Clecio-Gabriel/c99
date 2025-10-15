91143e9bc4846cd8a18c264e03cb1f212378b57e
Clecio-Gabriel <cleciogabofelix@gmail.com>
2025-10-15 18:42:20 -0300

    finished 1024_again.c

--- lista8/1024_again.c
+++ lista8/1024_again.c
@@ -14,34 +14,55 @@ int main(){
     char s[M];
     char res[N][M];
 
-    /*
-    como farei isso de forma mais eficiente?
-        primeiro, listemos o processo para compreender o que a atividade pede:
-            1. toda letra do alfabeto deve ser modificada em 3 posições para frente
-                char elem += 3
-            2. as linhas devem ser colocadas de trás para frente
-                precisa-se de uma função de reverse
-            3. todos os caracteres da metade pra frente devem ser movidos uma casa para esquerda
-                char elem -+ 1
-
-    funções que eu preciso:
-        encryption(string, iteração do i, matriz resultado) -- Função principal
-            vamo tentar fazer com a string.h
-        reverse(char string)
-    */
-
     for (int i = 0; i<N; i++){
         fgets(s, 1000, stdin);
         encryption(s, i, res);
     }
 
-    //TESTE
-    printf("%s", res[0]);
+    for (int i = 0; i<N; i++){
+        printf("%s\n", res[i]);
+    }
 
 }
 
-void reverse(char string [M]);
+void reverse(char string [M], int len);
+void ascii(char string[M], int len, int ini, int key, int op);
 
 void encryption(char s[M], int c, char res[][M]){
+    int size = strlen(s) - 1;
+
+    ascii(s, size, 0, 3, 1);
+    reverse(s, size);
+    ascii(s, size, (size/2), 1, 2);
+
     strcpy(res[c], s);  //não vai precisar mais da passresults
+}
+
+void ascii(char string[M], int len, int ini, int key, int op){
+
+    int ascii_num;
+    for (int i = ini; i<len; i++){
+        ascii_num = string[i];
+        switch(op){
+            case 1:
+            if (((ascii_num>=65)&&(ascii_num<=90))||((ascii_num>=97)&&ascii_num<=122)) string[i] += key;
+            break;
+            case 2:
+            string[i] -= key;
+        }
+    }   
+
+}
+
+void reverse(char string[M], int len){
+    char rev[M];
+
+    int j = len-1;
+    for (int i = 0; i<len; i++){
+        rev[i] = string[j];
+        j--;
+    }
+    rev[len] = '\0';
+
+    strcpy(string, rev);
 }
